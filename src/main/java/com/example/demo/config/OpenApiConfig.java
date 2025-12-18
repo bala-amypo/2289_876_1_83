231b73c0c5c025939c861e7be636a4c492623bbd
bala-amypo <bala@amypo.in>
2025-12-18 08:40:07 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,37 +1,20 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI openAPI() {
-
-        
-        SecurityScheme jwtScheme = new SecurityScheme()
-                .name("Authorization")
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Vendor SLA Performance Tracker API")
-                        .description("Backend API for tracking vendor SLA compliance, delivery performance, quality scores, and vendor tiers.")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("Procurement Team")
-                                .email("support@company.com")
-                        )
-                )
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .schemaRequirement("bearerAuth", jwtScheme);
-    }
-}
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9005.vs.amypo.ai")
+                ));
+        }
+}=
