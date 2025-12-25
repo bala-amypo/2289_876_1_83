88078db746364e0fbc611806814acb2b3f2728cb
bala-amypo <bala@amypo.in>
2025-12-25 15:30:18 +0000

    User commit

--- src/main/java/com/example/demo/VendorSlaPerformanceTrackerApplicationTests.java
+++ src/main/java/com/example/demo/VendorSlaPerformanceTrackerApplicationTests.java
@@ -1,12 +1,12 @@
-// package com.example.demo;
+package com.example.demo;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
-// class VendorSlaPerformanceTrackerApplicationTests {
+@SpringBootTest
+class VendorSlaPerformanceTrackerApplicationTests {
 
-//     @Test
-//     void contextLoads() {
-//     }
-// }
+    @Test
+    void contextLoads() {
+    }
+}
