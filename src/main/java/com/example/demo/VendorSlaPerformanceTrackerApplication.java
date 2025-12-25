a68e642f640b321420c0089a921504076aa4dd3a
bala-amypo <bala@amypo.in>
2025-12-25 17:01:03 +0000

    User commit

--- src/main/java/com/example/demo/VendorSlaPerformanceTrackerApplication.java
+++ src/main/java/com/example/demo/VendorSlaPerformanceTrackerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VendorSlaPerformanceTrackerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(VendorSlaPerformanceTrackerApplication.class, args);
     }
